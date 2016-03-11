7d4f213245847ac5ade45fc6b76d3a7547ced205
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-03-11 15:29:25 +0900

    update npm (version) and add code for use native cpp code

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = function() {
+//この状態だとなぜかfunctionが入ってる。なぜだ。。。
+
+// var expo={}
+
+// expo.helloworld= function() {
+//     console.log("we are modest.also npm again");
+// }
+// expo.native_addon=function(){
+//     var cpp=require("./native_addon/build/Release/helloworld");
+//     cpp.hello();
+// }
+
+// module.exports = expo;
+
+module.exports.hello_cpp=function() {
+    var cpp=require("./native_addon/build/Release/helloworld");
+    cpp.hello();
+}
+
+module.exports.hello_npm=function() {
     console.log("we are modest.also npm again");
 }
